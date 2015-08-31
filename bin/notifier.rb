99ab84ba23ea4781832f1eac9f3d6115d86b746f
Vinoth <a.vinothk@outlook.com>
2015-08-31 20:22:10 +0530

    post daily summary of commits

--- bin/notifier.rb
+++ bin/notifier.rb
@@ -8,18 +8,21 @@ def check_and_alert user
   puts "Checking if #{user} has pushed code for the day."
   resp = HTTParty.get(github_url)
   commits = JSON.parse(resp.body)
-  pushed = false
+  deployed_code = {}
   commits.each do |commit|
-    if pushed
+    if (Time.parse(commit['created_at']) + 330 * 60).day < Time.now.day
       break
+    else
+      deployed_code[commit['repo']['name']] = (deployed_code[commit['repo']['name']] || 0) + 1
     end
-    if Time.parse(commit['created_at']).day < Time.now.day and !(pushed)
-      message = "ALERT!!!!!! - Hey! @#{user}, You haven't pushed code today yet.. Gather up and do it NOW!!!"
-      post_message(URI::encode(message))
-      break
-    end
-    pushed = true
   end
+  if deployed_code.size < 1
+    message = "ALERT!!!!!! - Hey! @#{user}, You haven't pushed code today yet.. Gather up and do it NOW!!!"
+  else
+    message = "Summary: #{user} has committed #{deployed_code.values.inject(:+)} commits today. Breakup :- \n "
+    message += deployed_code.keys.map{|k| "#{k}: #{deployed_code[k]}"}.join("\n")
+  end
+  post_message URI::encode(message)
 end
 
 
@@ -36,7 +39,7 @@ users = ['avinoth', 'hindupuravinash']
 users.each do |user|
   puts "Checking commits of #{user}"
   # check_commit user
-  if Time.now.hour > 21
+  if Time.now.hour > 22
     check_and_alert user
   end
 end
